9ae51496095469128cf1d0afa3a2da073c65f9cf
joaovitorsouza007 <150279933+joaovitorsouza007@users.noreply.github.com>
2025-03-04 11:48:40 -0300

    Adição do alert personalizado
    aaa

--- Controller/cadastroEmpresa.act.php
+++ Controller/cadastroEmpresa.act.php
@@ -1,42 +1,37 @@
 <?php 
-
-
 require("../model/connect.php");
 extract($_POST);
 extract($_FILES);
 @session_start();
 
 $dir = "../fotosSite/" .md5(time()) . ".jpg";
+
 if(!file_exists("../fotosSite/")){
     mkdir("../fotosSite/", 0777, true);
 }
+
 $busca = mysqli_query($con, "SELECT * FROM `empresa` WHERE `email` = '$email'");
-
-
 
 if($busca->num_rows == 0){
     $senha = password_hash($senha,PASSWORD_DEFAULT);
+    
     if(mysqli_query($con, "INSERT INTO empresa (nome_empresa,cnpj, cep,email, senha, logo,ddd, telefone,ie) VALUES('$nome', '$cnpj' ,'$cep','$email','$senha', '$dir', null, '$telefone','$ie');")){
         move_uploaded_file($logo['tmp_name'],$dir);
-        $_SESSION["msg"] = "Sucessoooo";
-    }else{
-        $_SESSION["msg"] = "erro!";
+        $_SESSION['msg'] = "Sucesso!";
+        $_SESSION['alertMsg'] = "Cadastro Realizado!";
+        $_SESSION['alertIcon'] = "success";
+    } else {
+        $_SESSION['msg'] = "Erro!";
+        $_SESSION['alertMsg'] = "Erro ao Cadastrar!";
+        $_SESSION['alertIcon'] = "error";
     }
 
-}else{
-    $_SESSION["msg"] = "Email já existente";
+} else {
+    $_SESSION['msg'] = "Email já existente!";
+    $_SESSION['alertMsg'] = "Este email já está cadastrado!";
+    $_SESSION['alertIcon'] = "error";
 }
 
-
-header("location:../View/cadastroEmpresa.php");
-
-
-
-
-
-
-
-
-
-
+header("Location: ../View/cadastroEmpresa.php");
+exit();
 ?>
--- View/cadastroEmpresa.php
+++ View/cadastroEmpresa.php
@@ -1,3 +1,8 @@
+<?php
+@session_start();
+?>
+
+<!DOCTYPE html>
 <html lang="pt-br">
   <head>
     <meta charset="UTF-8" />
@@ -7,131 +12,133 @@
     <?php include("parts/head.php") ?>
     <script src="../js/jquery.mask.js"></script>
     <link rel="stylesheet" href="../Css/geral.css">
-    <title>Cadastro</title>
-    <script src="../js/jquery.mask.js"></script>
-    <script src="../js/main.js"> </script>
+    <script src="../js/main.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
   </head>
 
-  
   <body>
-    <p class="message">
-          <?php
-  
-      @session_start();
-          if (isset($_SESSION["msg"])) {
-              echo $_SESSION["msg"];
-              unset($_SESSION["msg"]);
-          }
-  
-  
-          ?>
-      </p>
+
+    <?php
+    if (isset($_SESSION["msg"])) {
+        echo "<script>
+            Swal.fire({
+                title: '{$_SESSION['msg']}',
+                text: '{$_SESSION['alertMsg']}',
+                icon: '{$_SESSION['alertIcon']}'
+            });
+        </script>";
+        unset($_SESSION["msg"]);
+        unset($_SESSION["alertMsg"]);
+        unset($_SESSION["alertIcon"]);
+    }
+    ?>
+
     <div id="telaCadastro">
-      <!-- Coluna do Ícone -->
       <div id="iconeCadastro">
         <p>Faça seu cadastro</p>
         <img src="../Imagens/imagemEmpresa.png" alt="Ícone" id="imagemEmpresa">
       </div>
-      <!-- Coluna do Formulário -->
+
       <form id="cadastro" action="../Controller/cadastroEmpresa.act.php" method="post" enctype="multipart/form-data" name="cadastroEmpresa">
         <div class="container">
           <div class="row">
-          <label for="nome">
-            Nome Empresa
-            <input type="text" id="nome" name="nome" required>
-          </label>
-
-           <label for="cnpj">
-            CNPJ
-            <input type="text" id="cnpj" name="cnpj" placeholder="00.000.000.0000-00"/>
-          </label>
-          </div>
-
-          <div class="row">
-          <label for="ie">
-            IE (Inscrição Estadual)
-            <input type="text" id="ie" name="ie" placeholder="000.000.000.000"/>
-          </label>
- 
-          <label for="telefone">
-            Telefone
-            <input type="text" id="telefone" name="telefone" placeholder="(00) 0000-0000"/>
-          </label> 
-        </div>
-
-          <div class="row">
-          <div class="cep">
-            <label for="cep">
-              CEP
-              <input type="text" id="cep" name="cep" placeholder="00000-000" />
+            <label for="nome">
+              Nome Empresa
+              <input type="text" id="nome" name="nome" required>
             </label>
-            <a href="https://buscacepinter.correios.com.br/app/endereco/index.php" target="_blank" rel="noopener noreferrer">Não sabe o CEP?</a>
-          </div>
 
-          <label for="rua">
-            Rua
-            <input type="text" id="rua" disabled />
-          </label>
-          </div>
-
-          <div class="row">
-          <label for="bairro">
-            Bairro
-            <input type="text" id="bairro" disabled />
-          </label>
-
-          <label for="cidade">
-            Cidade
-            <input type="text" id="cidade" disabled />
-          </label>
-
-          <label for="estado">
-            Estado
-            <input type="text" id="estado" disabled />
-          </label>
+            <label for="cnpj">
+              CNPJ
+              <input type="text" id="cnpj" name="cnpj" placeholder="00.000.000.0000-00"/>
+            </label>
           </div>
 
           <div class="row">
-          <label for="email">
-            Email
-            <input type="email" id="email" name="email" placeholder="digite seu email" required>
-          </label>
+            <label for="ie">
+              IE (Inscrição Estadual)
+              <input type="text" id="ie" name="ie" placeholder="000.000.000.000"/>
+            </label>
 
-          <label for="senha">
-            Senha
-            <input type="password" id="senha" name="senha" required>
-          </label>
-
-        </div>
-        <div class="row">
-
-          <label for="file" class="file">
-            <div class="icone">
-           <img src="../icones/file.svg" alt="">
+            <label for="telefone">
+              Telefone
+              <input type="text" id="telefone" name="telefone" placeholder="(00) 0000-0000"/>
+            </label> 
           </div>
-            <p>Inserir Arquivo</p>
-            <input type="file" name="logo" id="file" required>
-          </label>
 
-          <button class="enviar" type="submit"> 
-            <img src="../icones/Send.svg" alt="">
-            Cadastrar</button>
-        </div>
+          <div class="row">
+            <div class="cep">
+              <label for="cep">
+                CEP
+                <input type="text" id="cep" name="cep" placeholder="00000-000" />
+              </label>
+              <a href="https://buscacepinter.correios.com.br/app/endereco/index.php" target="_blank" rel="noopener noreferrer">Não sabe o CEP?</a>
+            </div>
+
+            <label for="rua">
+              Rua
+              <input type="text" id="rua" disabled />
+            </label>
+          </div>
+
+          <div class="row">
+            <label for="bairro">
+              Bairro
+              <input type="text" id="bairro" disabled />
+            </label>
+
+            <label for="cidade">
+              Cidade
+              <input type="text" id="cidade" disabled />
+            </label>
+
+            <label for="estado">
+              Estado
+              <input type="text" id="estado" disabled />
+            </label>
+          </div>
+
+          <div class="row">
+            <label for="email">
+              Email
+              <input type="email" id="email" name="email" placeholder="Digite seu email" required>
+            </label>
+
+            <label for="senha">
+              Senha
+              <input type="password" id="senha" name="senha" required>
+            </label>
+          </div>
+
+          <div class="row">
+            <label for="file" class="file">
+              <div class="icone">
+                <img src="../icones/file.svg" alt="">
+              </div>
+              <p>Inserir Arquivo</p>
+              <input type="file" name="logo" id="file" required>
+            </label>
+
+            <button class="enviar" type="submit"> 
+              <img src="../icones/Send.svg" alt="">
+              Cadastrar
+            </button>
+          </div>
         </div>
       </form>
     </div>
 
     <script type="text/javascript">
-      //mascaras
       $(document).ready(function () {
         $("#telefone").mask("99 9999-9999");
         $("#whatsapp").mask("99 99999-9999");
         $("#cnpj").mask("00.000.000/0000-00", { reverse: true });
         $("#cep").mask("00000-000");
         $("#ie").mask("000.000.000.000");
-        })
+      });
     </script>
+
     <script src="../js/cep.js"></script>
     <script src="../js/file.js"></script>
+
   </body>
 </html>
