6407991f5422925901e6a83c18a444668b2bc317
joaovitorsouza007 <150279933+joaovitorsouza007@users.noreply.github.com>
2025-03-25 14:13:05 -0300

    Curso

---
+++ View/cursos.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LearnFY</title>
+    <?php include("parts/head.php") ?>
+    <link rel="stylesheet" href="../Css/cursos.css">
+<body>
+<?php 
+    include("parts/header.php");
+?>
+
+    <div id="principal">
+
+    <div id="bar"><h1>Cursos $</h1></div>
+    <div class="pesquisar">
+        <div class="search-box">
+            <span class="search-icon">Q</span>
+            <input type="text" placeholder="Pesquisar curso" class="search-input">
+        </div>
+    </div>
+
+    <div class="categorias">
+
+        <h1>$categoria</h1>
+        <div>
+
+            <div class="cursos">
+                <img src="../fotosSite/image 62.png" alt="">
+                <div><h1>$categoria</h1></div>
+                <h1>$nomeCurso</h1>
+                <div><p>Oferecido por:</p> <img src="" alt=""></div>
+                <div><img src="../icones/image 8.png" alt=""> <p>500</p> <button>$situacao</button></div>
+            </div>
+
+        </div>
+
+    </div>
+
+    </div>
+
+<?php 
+    include("parts/footer.php");
+?>
