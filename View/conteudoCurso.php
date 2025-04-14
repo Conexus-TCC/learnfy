4bcbaf66ec9ae2a9d52206eafb53d3a4e7cee593
joaovitorsouza007 <150279933+joaovitorsouza007@users.noreply.github.com>
2025-04-14 14:25:16 -0300

    Conteudo curso em andamento
    Criação da parte de adicionar conteudo nos cursos(incompleto)

---
+++ View/conteudoCurso.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LearnFY</title>
+    <?php include("parts/head.php") ?>
+    <link rel="stylesheet" href="../Css/contCurso.css">
+<body>
+<?php 
+    include("parts/header.php");
+?>
+<div id="principal">
+
+    <div id="esq">
+
+        <div id="contCurso">
+            <h1>Conteúdo do curso</h1>
+            <p>4 Aulas - 480 Minutos</p>
+
+            <div class="aulas">
+                <a href="">
+                <div><img src="../icones/play.png" alt=""></div>
+                <div>
+                    <h1>Introdução ao HTML5</h1> <p>45 min</p>
+                </div>
+            </a>
+            </div>
+
+            <div class="aulas">
+            <a href="">
+                <div><img src="../icones/play.png" alt=""></div>
+                <div>
+                    <h1>Introdução ao HTML5</h1> <p>45 min</p>
+                </div>
+            </a>
+            </div>
+            
+        </div>
+
+    </div>
+
+    
+    <div id="dir">
+        <div id="barra">
+
+            <div>
+                <h1>Desenvolvimento Web Completo</h1>
+                <p>Aula 1: Introdução ao HTML5</p>
+            </div>
+
+            <div id="btn">
+                <button>Anterior</button>
+                <button>Próxima</button>
+            </div>
+
+        </div>
+
+        <div id="cont">
+            <a href=""><img src="../icones/arquivo-de-video.png" alt=""> <p></p>Vídeo</a>
+            <a href=""><img src="../icones/arquivos.png" alt=""> <p>Matériais</p></a>
+            <a href=""><img src="../icones/correto.png" alt=""> <p>Quiz</p></a>
+        </div>
+ 
+        <div id="video">
+            <video src=""></video>
+        </div>
+
+    </div>
+
+</div>
+
+</body>
