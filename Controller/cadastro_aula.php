78417bac851b3012f49f15cff80824743fc819af
Eu <emanuel.dosmelo@gmail.com>
2025-05-21 15:16:01 -0300

    adiciona o javascript para acriacao de quizes e requesição

--- Controller/cadastro_aula.php
+++ Controller/cadastro_aula.php
@@ -1,86 +1,89 @@
 <?php
-require_once "../Model/connect.php";
-@session_start();
-// header("Content-Type: application/json; charset=UTF-8");
+//  echo var_dump($_POST);
+echo json_encode($_POST);
+// require_once "../Model/connect.php";
+// @session_start();
+// // header("Content-Type: application/json; charset=UTF-8");
 
-$id_curso = $_SESSION["id_curso"];
-/*
-em caso de erro executar o seguinte comando no banco de dados:
-*/
-function err($id_curso,$con){
-  $query = mysqli_query($con,"SELECT id_aula FROM `aula` where id_curso = '$id_curso'" );
-  $arr= array();
-  while($a =$query->fetch_assoc()){
-    array_push($arr,$a["id_aula"]);
-  }
+// $id_curso = $_SESSION["id_curso"];
+// /*
+// em caso de erro executar o seguinte comando no banco de dados:
+// */
+// function err($id_curso,$con){
+//   $query = mysqli_query($con,"SELECT id_aula FROM `aula` where id_curso = '$id_curso'" );
+//   $arr= array();
+//   while($a =$query->fetch_assoc()){
+//     array_push($arr,$a["id_aula"]);
+//   }
   
-  foreach ($arr as $key => $idAula) {
-    $a =  mysqli_query($con,"SELECT caminho FROM `materiais_aula` WHERE id_aula = '$idAula' ");
-    if($a->num_rows>0){
-      while (($row = $a->fetch_assoc()) != null) {
-        @unlink($row["caminho"]);
-      }
-    }
-    $sqlErr1 = " DELETE FROM `materiais_aula` WHERE `id_aula` = '$idAula'";
-    $sqlErr2 = "DELETE FROM `aula` WHERE `id_aula` = '$idAula'";
-    mysqli_query($con,$sqlErr1);
-    mysqli_query($con,$sqlErr2);
-    mysqli_error($con);
-  }
-}
-json_encode($_FILES);
-extract($_POST);
-extract($_FILES);
+//   foreach ($arr as $key => $idAula) {
+//     $a =  mysqli_query($con,"SELECT caminho FROM `materiais_aula` WHERE id_aula = '$idAula' ");
+//     if($a->num_rows>0){
+//       while (($row = $a->fetch_assoc()) != null) {
+//         @unlink($row["caminho"]);
+//       }
+//     }
+//     $sqlErr1 = " DELETE FROM `materiais_aula` WHERE `id_aula` = '$idAula'";
+//     $sqlErr2 = "DELETE FROM `aula` WHERE `id_aula` = '$idAula'";
+//     mysqli_query($con,$sqlErr1);
+//     mysqli_query($con,$sqlErr2);
+//     mysqli_error($con);
+//   }
+// }
+// json_encode($_FILES);
+// extract($_POST);
+// extract($_FILES);
 
-foreach ($_POST as $key => $value) {
-  if(!isset($_POST[$key])|| $_POST[$key] == "" ){
-    echo json_encode(array("msg" => "error", "alertMsg" => "o campo $key esta faltando", "alertIcon" => "error"));
-    http_response_code("400");  
-    err($id_curso,$con);
-    exit();
-  }
-}
-if(!isset($video_aula)){
-  echo json_encode(array("msg" => "error", "alertMsg" => "A aula de conter um video - aula", "alertIcon" => "error"));
-  http_response_code("400");
-  err($id_curso, $con);
-  exit();
-}
-if(!str_contains($video_aula['type'], 'video/')){
-  err($id_curso, $con);
-   echo json_encode(array("msg"=>"error","alertMsg"=>"Formato de video inválido!","alertIcon"=>"error"));
-    http_response_code("400");
-    exit();
-}
-$caminhoVideo = "../fotosSite/". md5(time()) .$id_curso . ".mp4";
+// foreach ($_POST as $key => $value) {
+//   if(!isset($_POST[$key])|| $_POST[$key] == "" ){
+//     echo json_encode(array("msg" => "error", "alertMsg" => "o campo $key esta faltando", "alertIcon" => "error"));
+//     http_response_code("400");  
+//     err($id_curso,$con);
+//     exit();
+//   }
+// }
+// if(!isset($video_aula)){
+//   echo json_encode(array("msg" => "error", "alertMsg" => "A aula de conter um video - aula", "alertIcon" => "error"));
+//   http_response_code("400");
+//   err($id_curso, $con);
+//   exit();
+// }
+// if(!str_contains($video_aula['type'], 'video/')){
+//   err($id_curso, $con);
+//    echo json_encode(array("msg"=>"error","alertMsg"=>"Formato de video inválido!","alertIcon"=>"error"));
+//     http_response_code("400");
+//     exit();
+// }
+// $caminhoVideo = "../fotosSite/". md5(time()) .$id_curso . ".mp4";
 
-if(mysqli_query($con,"INSERT INTO `aula` (`nome`, `descricao`, `id_curso`, `video`) 
-VALUES ('$titulo_aula', '$descricao_aula', '$id_curso','$caminhoVideo') ")){
-$id_aula= mysqli_insert_id($con);
-@move_uploaded_file($video_aula['tmp_name'],$caminhoVideo);
-$cont = count($input_materiais["name"]);
-for($i=0;$i<$cont;$i++){
-  if($input_materiais["name"][$i]!=""){
-    $ext = pathinfo($input_materiais["full_path"][$i], PATHINFO_EXTENSION);
-    $caminhoMaterial = "../fotosSite/". md5(time()) .$id_curso . ".".$ext;
-    $filename = $input_materiais["name"][$i];
-    $sql = "INSERT INTO `materiais_aula` (`filename`, `caminho`, `id_aula`) VALUES ('$filename', '$caminhoMaterial', '$id_aula')";
-    if(mysqli_query($con,$sql)){
-      @move_uploaded_file($input_materiais["tmp_name"][$i],$caminhoMaterial);
-    }else{
-      echo json_encode(array("msg"=>"error","alertMsg"=>"Erro ao cadastrar o material!","alertIcon"=>"error"));
-      http_response_code("400");
-      err($id_curso, $con);
-      exit();
-    }
+// if(mysqli_query($con,"INSERT INTO `aula` (`nome`, `descricao`, `id_curso`, `video`) 
+// VALUES ('$titulo_aula', '$descricao_aula', '$id_curso','$caminhoVideo') ")){
+// $id_aula= mysqli_insert_id($con);
+// @move_uploaded_file($video_aula['tmp_name'],$caminhoVideo);
+// $cont = count($input_materiais["name"]);
+// for($i=0;$i<$cont;$i++){
+//   if($input_materiais["name"][$i]!=""){
+//     $ext = pathinfo($input_materiais["full_path"][$i], PATHINFO_EXTENSION);
+//     $caminhoMaterial = "../fotosSite/". md5(time()) .$id_curso . ".".$ext;
+//     $filename = $input_materiais["name"][$i];
+//     $sql = "INSERT INTO `materiais_aula` (`filename`, `caminho`, `id_aula`) VALUES ('$filename', '$caminhoMaterial', '$id_aula')";
+//     if(mysqli_query($con,$sql)){
+//       @move_uploaded_file($input_materiais["tmp_name"][$i],$caminhoMaterial);
+//     }else{
+//       echo json_encode(array("msg"=>"error","alertMsg"=>"Erro ao cadastrar o material!","alertIcon"=>"error"));
+//       http_response_code("400");
+//       err($id_curso, $con);
+//       exit();
+//     }
 
-  }
-}
-  echo json_encode(array("msg" => "success", "alertMsg" => "Aula cadastrada com sucesso!", "alertIcon" => "success"));
-  http_response_code("200");
-}else{
-  err($id_curso, $con);
- echo json_encode(array("msg" => "error", "alertMsg" => "Erro ao cadastrar a aula!", "alertIcon" => "error"));
-  http_response_code("400");
-  exit();
-}
+//   }
+// }
+//   echo json_encode(array("msg" => "success", "alertMsg" => "Aula cadastrada com sucesso!", "alertIcon" => "success"));
+//   http_response_code("200");
+// }else{
+//   err($id_curso, $con);
+//  echo json_encode(array("msg" => "error", "alertMsg" => "Erro ao cadastrar a aula!", "alertIcon" => "error"));
+//   http_response_code("400");
+//   exit();
+// }
+
--- js/addAula.js
+++ js/addAula.js
@@ -1,14 +1,14 @@
-const btn = document.querySelector(".btn-Add-Aula");
-let i = 0;
+const btnCriarAula = document.querySelector(".btn-Add-Aula");
+
+
 const enviar = document.querySelector("#enviar");
 const aulas = document.querySelector(".aulas");
-btn.addEventListener("click", function () {
-  i++;
+btnCriarAula.addEventListener("click", function () {
+  let i = document.querySelectorAll(".campos-Curso").length+1;
   const evento = new CustomEvent("criar-aula", { detail: { index: i } });
   console.log(evento.detail);
   const aula = document.createElement("div");
-  aula.innerHTML = `
-  <form method="POST" enctype="multipart/form-data" id="form-${i}" class="campos-Curso">
+  aula.innerHTML = ` <form method="POST" enctype="multipart/form-data" id="form-${i}" class="campos-Curso">
     <div class="cabecalho">
       <div class="flex">
         <div class="bola">
@@ -17,15 +17,15 @@ btn.addEventListener("click", function () {
         <span>Aula ${i}</span>
       </div>
       <div class="btns">
-        <button class="btn-delete" onclick="deletar(${i},this)" type="button">
-        <svg width="48" height="48" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <g id="SVGRepo_iconCarrier" stroke="#92386B" >
-            <path
-              d="M10 12L14 16M14 12L10 16M18 6L17.1991 18.0129C17.129 19.065 17.0939 19.5911 16.8667 19.99C16.6666 20.3412 16.3648 20.6235 16.0011 20.7998C15.588 21 15.0607 21 14.0062 21H9.99377C8.93927 21 8.41202 21 7.99889 20.7998C7.63517 20.6235 7.33339 20.3412 7.13332 19.99C6.90607 19.5911 6.871 19.065 6.80086 18.0129L6 6M4 6H20M16 6L15.7294 5.18807C15.4671 4.40125 15.3359 4.00784 15.0927 3.71698C14.8779 3.46013 14.6021 3.26132 14.2905 3.13878C13.9376 3 13.523 3 12.6936 3H11.3064C10.477 3 10.0624 3 9.70951 3.13878C9.39792 3.26132 9.12208 3.46013 8.90729 3.71698C8.66405 4.00784 8.53292 4.40125 8.27064 5.18807L8 6"
-              stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
-          </g>
-        </svg>
-      </button>
+        <button class="btn-delete" onclick="deletar('#form-${i}')" type="button">
+          <svg width="48" height="48" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g id="SVGRepo_iconCarrier" stroke="#92386B">
+              <path
+                d="M10 12L14 16M14 12L10 16M18 6L17.1991 18.0129C17.129 19.065 17.0939 19.5911 16.8667 19.99C16.6666 20.3412 16.3648 20.6235 16.0011 20.7998C15.588 21 15.0607 21 14.0062 21H9.99377C8.93927 21 8.41202 21 7.99889 20.7998C7.63517 20.6235 7.33339 20.3412 7.13332 19.99C6.90607 19.5911 6.871 19.065 6.80086 18.0129L6 6M4 6H20M16 6L15.7294 5.18807C15.4671 4.40125 15.3359 4.00784 15.0927 3.71698C14.8779 3.46013 14.6021 3.26132 14.2905 3.13878C13.9376 3 13.523 3 12.6936 3H11.3064C10.477 3 10.0624 3 9.70951 3.13878C9.39792 3.26132 9.12208 3.46013 8.90729 3.71698C8.66405 4.00784 8.53292 4.40125 8.27064 5.18807L8 6"
+                stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+            </g>
+          </svg>
+        </button>
         <button class="btn-collapse" onclick="colapsar(${i},this)" style="rotate:0deg;transiton:0.5s" type="button">
           <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
             <g id="Arrow down" stroke="#985C7E">
@@ -36,7 +36,7 @@ btn.addEventListener("click", function () {
         </button>
       </div>
     </div>
-    <div class="campos a${i} colapsed">
+    <div class="campos a${i}">
       <label for="titulo-aula-${i}">
         <p>Titulo da aula</p>
         <input type="text" name="titulo_aula" id="titulo-aula-${i}">
@@ -75,27 +75,34 @@ btn.addEventListener("click", function () {
           <div class="materiais-selecionados">
           </div>
         </div>
-        <script>
 
-        </script>
-
+      </div>
+      <div class="quiz">
+        <p>Questões</p>
+        <button id="AdicionarQuizCurso" type="button" class="caixaAdicionar">
+          <svg width="20" height="20" viewBox="0 0 384 380" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M192 79.167V300.834M80 190H304" stroke="#92386B" stroke-width="50" stroke-linecap="round" stroke-linejoin="round" />
+          </svg>
+          <p>Adicionar Questão</p>
+        </button>
+        <div class="container">
+        </div>
       </div>
     </div>
 
-  </form>
-  `;
+  </form>  `;
   aulas.appendChild(aula);
   dispatchEvent(evento);
   scrollTo({ top: aulas.scrollHeight, behavior: "smooth" });
 });
 enviar.addEventListener("click", async (e) => {
   e.preventDefault();
-  e.target.disable=true;
+  e.target.disable = true;
   const forms = document.querySelectorAll(".campos-Curso");
-let  i =0
-let success = true;
+  let i = 0;
+  let success = true;
   for (const form of forms) {
-    i++
+    i++;
     console.log(form);
     const formData = new FormData();
     const titulo = form.querySelector("#titulo-aula-" + i).value;
@@ -108,18 +115,44 @@ let success = true;
     for (const material of materiais) {
       formData.append("input_materiais[]", material);
     }
+    const questoes = form.querySelectorAll(".questao");
+    let j =0
+    questoes.forEach((questao, index) => {
+     j++
+      const pergunta = questao.querySelector(
+        `input[name='pergunta[]']`
+      ).value;
+      const respostas = Array.from(
+        questao.querySelectorAll(`input[name='resposta${j}[]']`)
+      ).map((input) => input.value);
+      const respostaCorretaIndex = Array.from(
+        questao.querySelectorAll(`input[name='respostaCheck${j}']`)
+      ).findIndex((input) => input.checked);
+
+      formData.append(`questoes[${index}][pergunta]`, pergunta);
+      respostas.forEach((resposta, respostaIndex) => {
+        formData.append(`questoes[${index}][respostas][${respostaIndex}]`, resposta);
+      });
+      formData.append(`questoes[${index}][respostaCorreta]`, respostaCorretaIndex);
+      console.log(j)
+      console.log(questao)
+      console.log(respostas)
+      console.log(pergunta)
+      console.log(respostaCorretaIndex)
     
+    });
+
     $.ajax({
       type: "POST",
       url: "../../controller/cadastro_aula.php",
       data: formData,
       processData: false,
       contentType: false,
-      error:function (response) {
-        success= false
-        console.log( response)
-        const {responseJSON} = response 
-        if(responseJSON){
+      error: function (response) {
+        success = false;
+        console.log(response);
+        const { responseJSON } = response;
+        if (responseJSON) {
           Swal.fire({
             icon: responseJSON.alertIcon,
             title: responseJSON.msg,
@@ -127,35 +160,33 @@ let success = true;
           }).then(() => {
             form.querySelector(".btn-collapse").click();
           });
-        }else{
+        } else {
           Swal.fire({
             icon: "error",
-            title:"Arquivos muito grande ",
+            title: "Arquivos muito grande ",
             text: "O video e/ou  os materias superam o limite de 40MB",
           }).then(() => {
             form.querySelector(".btn-collapse").click();
           });
         }
-      e.target.disable = true;  
+        e.target.disable = true;
       },
       success: function (response) {
         console.log(response);
-        if((i-1)<forms.length&&success){
-           Swal.fire({
-             icon: "success",
-             title: "Aulas cadastradas com sucesso!",
-             text: "🎉🥳",
-           }).then(() => {
-             window.location.href = "./dashboard.php";
-           });
-          return
+        if (i - 1 < forms.length && success) {
+          Swal.fire({
+            icon: "success",
+            title: "Aulas cadastradas com sucesso!",
+            text: "🎉🥳",
+          }).then(() => {
+           // window.location.href = "./dashboard.php";
+          });
+          return;
         }
       },
     });
-     if(success==false ) break
-}
-  
-  ;
+    if (success == false) break;
+  }
 });
 window.addEventListener("criar-aula", function (e) {
   const { index: i } = e.detail;
@@ -197,6 +228,7 @@ window.addEventListener("criar-aula", function (e) {
   console.log(e);
   const { index: i } = e.detail;
   console.log(i);
+  ovidorCurso(`#form-${i}`);
   const input = document.getElementById(`input-materiais-${i}`);
   const divMateriais = document.querySelector(
     "#sla" + i + " .materiais-selecionados"
@@ -212,3 +244,53 @@ window.addEventListener("criar-aula", function (e) {
     }
   });
 });
+
+
+function ovidorCurso(contexto) {
+  const btnQuiz = document.querySelector( contexto+" #AdicionarQuizCurso");
+  btnQuiz.addEventListener("click", (e) => {
+    let k = document.querySelectorAll(contexto+" .questao").length + 1;
+    const container = document.querySelector(contexto+" .container");
+    const questao = document.createElement("div");
+    questao.classList.add("questao");
+    questao.id = "questao" + k;
+    questao.innerHTML = `
+            <div class="header">
+              <div class="bola">${k}</div>
+              <input type="text" id="pergunta" placeholder="insira a questão" name="pergunta[]">
+              <button type="button" class="btn-delete" onclick="deletar('#questao${k}')">
+              <svg width="48" height="48" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g id="SVGRepo_iconCarrier" stroke="#92386B">
+              <path
+                d="M10 12L14 16M14 12L10 16M18 6L17.1991 18.0129C17.129 19.065 17.0939 19.5911 16.8667 19.99C16.6666 20.3412 16.3648 20.6235 16.0011 20.7998C15.588 21 15.0607 21 14.0062 21H9.99377C8.93927 21 8.41202 21 7.99889 20.7998C7.63517 20.6235 7.33339 20.3412 7.13332 19.99C6.90607 19.5911 6.871 19.065 6.80086 18.0129L6 6M4 6H20M16 6L15.7294 5.18807C15.4671 4.40125 15.3359 4.00784 15.0927 3.71698C14.8779 3.46013 14.6021 3.26132 14.2905 3.13878C13.9376 3 13.523 3 12.6936 3H11.3064C10.477 3 10.0624 3 9.70951 3.13878C9.39792 3.26132 9.12208 3.46013 8.90729 3.71698C8.66405 4.00784 8.53292 4.40125 8.27064 5.18807L8 6"
+                stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+            </g>
+          </svg>
+              </button>
+            
+              </div>
+            <div class="inputs">
+              <div class="radios">
+                <div class="resposta">
+                  <input type="radio" checked name="respostaCheck${k}" id="">
+                  <input type="text" name="resposta${k}[]" placeholder="resposta 1">
+                </div>
+                <div class="resposta">
+                  <input type="radio" name="respostaCheck${k}" id="">
+                  <input type="text" name="resposta${k}[]" placeholder="resposta 2">
+                </div>
+                <div class="resposta">
+                  <input type="radio" name="respostaCheck${k}" id="">
+                  <input type="text" name="resposta${k}[]" placeholder="resposta 3">
+                </div>
+                <div class="resposta">
+                  <input type="radio" name="respostaCheck${k}" id="">
+                  <input type="text" name="resposta${k}[]" placeholder="resposta 4">
+                </div>
+              </div>
+            </div>
+  `;
+    container.appendChild(questao);
+  });
+}
+
