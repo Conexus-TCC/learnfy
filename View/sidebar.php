4d15846ff839c0150ace6eb52cf190365723bf22
Rick nelber <ricardo.nelber.70@gmail.com>
2025-05-23 15:26:01 -0300

    Integração do Button da sidebar

--- View/sidebar.php
+++ View/sidebar.php
@@ -1,12 +1,8 @@
-<link rel="stylesheet" href="/learnfy/Css/sidebar.css">
 <aside class="sidebar" id="sidebar">
   <div class="sidebar-header">
     <div class="sidebar-logo">
       <div class="sidebar-logo-icon"></div>
-      <h1><?php
-          @session_start();
-          echo $_SESSION["nome"];
-          ?></h1>
+      <h1>LearnFY</h1>
     </div>
     <button class="sidebar-toggle" id="sidebar-toggle">
       &#9776;
@@ -15,21 +11,21 @@
 
   <ul class="sidebar-menu">
     <li class="sidebar-menu-item">
-      <a onclick="trocarSite('dashboard.php',this)" class="sidebar-menu-link active">
+      <a href="gerenciamento.php" class="sidebar-menu-link ">
         <span class="sidebar-menu-icon">📊</span>
         <span class="sidebar-menu-text">Dashboard</span>
       </a>
       <div class="tooltip">Dashboard</div>
     </li>
     <li class="sidebar-menu-item">
-      <a onclick="trocarSite('cursos.php',this)" class="sidebar-menu-link">
+      <a href="#" class="sidebar-menu-link">
         <span class="sidebar-menu-icon">📚</span>
         <span class="sidebar-menu-text">Cursos</span>
       </a>
       <div class="tooltip">Cursos</div>
     </li>
     <li class="sidebar-menu-item">
-      <a onclick="trocarSite('colaboradores.php',this)" class="sidebar-menu-link">
+      <a href="colaboradores.php" class="sidebar-menu-link active">
         <span class="sidebar-menu-icon">👥</span>
         <span class="sidebar-menu-text">Colaboradores</span>
       </a>
@@ -43,22 +39,12 @@
       <div class="tooltip">Relatórios</div>
     </li>
     <li class="sidebar-menu-item">
-      <a onclick="trocarSite('editarEmpresa.php',this)" class="sidebar-menu-link">
+      <a href="#" class="sidebar-menu-link">
         <span class="sidebar-menu-icon">🏢</span>
         <span class="sidebar-menu-text">Empresa</span>
       </a>
       <div class="tooltip">Empresa</div>
     </li>
-
-    <li class="sidebar-menu-item">
-      <a onclick="trocarSite('categorias.php',this)" class="sidebar-menu-link">
-        <span class="sidebar-menu-icon">🔣</span>
-        <span class="sidebar-menu-text">categoria Cursos</span>
-      </a>
-      <div class="tooltip">Empresa</div>
-    </li>
-
-
     <li class="sidebar-menu-item">
       <a href="#" class="sidebar-menu-link">
         <span class="sidebar-menu-icon">⚙️</span>
@@ -69,25 +55,9 @@
   </ul>
 
   <div class="sidebar-footer">
-    
-    <a href="../../View/index.php" class="logoff-btn">
-      <span class="sidebar-menu-icon">🔄</span>
-      <span class="logoff-btn-text">Voltar</span>
-    </a>
-  
-    <a href="../../Controller/logout.php" class="logoff-btn">
+    <button class="logoff-btn">
       <span class="sidebar-menu-icon">🚪</span>
       <span class="logoff-btn-text">Sair</span>
-    </a>
-    
+    </button>
   </div>
 </aside>
-<script>
-  // Toggle sidebar
-  const sidebar = document.getElementById('sidebar');
-  const sidebarToggle = document.getElementById('sidebar-toggle');
-
-  sidebarToggle.addEventListener('click', () => {
-    sidebar.classList.toggle('collapsed');
-  });
-</script>
