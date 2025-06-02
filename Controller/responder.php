790a4daf2d6007f621b57c04b68689b02fe3e1f2
Eu <emanuel.dosmelo@gmail.com>
2025-06-02 17:13:22 -0300

    sla

--- Controller/responder.php
+++ Controller/responder.php
@@ -7,7 +7,7 @@ $sql = "SELECT id_res_certa FROM `pergunta` where id_pergunta = $idPergunta";
 $res =  mysqli_query($con,$sql)->fetch_assoc();
 if($res["id_res_certa"]==$resposta){
   if(mysqli_query($con, "INSERT INTO `progresso` (`id_usuario`, `id_pergunta`, `id_quiz`) VALUES ('$_SESSION[id_usuario]', '$idPergunta', '$idQuiz') ")){
-    echo json_encode(array("status"=>true));
+    echo json_encode(array("status"=>true, "resp" => $resposta));
   }else{
     echo json_encode(array("status" => false,"motivo"=>"Erro interno","resp"=>$resposta));
   }
