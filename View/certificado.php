8cef0f9df8dba5d872cba1c7e16e3ee5f192915c
Eu <emanuel.dosmelo@gmail.com>
2025-06-21 11:40:32 -0300

    front end do certificado

--- View/certificado.php
+++ View/certificado.php
@@ -11,7 +11,8 @@ if (!isset($_GET["id"])) {
 }
 $query = "SELECT empresa.nome_empresa,
 usuario.nome_usuario,
-curso.nome as curso_nome,
+curso.nome as nome_curso,
+empresa.logo,
 certificado.data_emissao,
 SUM(aula.tempo_em_segundos)as tempo
 FROM certificado 
@@ -21,21 +22,44 @@ left JOIN curso on curso.id_curso = certificado.id_curso
 left JOIN aula on aula.id_curso = certificado.id_curso
 WHERE certificado.id_certificado=$_GET[id]";
 $result = mysqli_query($con, $query)->fetch_assoc();
+$svg = file_get_contents("../icones/selo.svg");
+function horaOuMinutos($horas)
+{
+  if (round($horas / 3600) > 0) {
+    return round($horas / 3600) . " hora(s)";
+  } else {
+    return round($horas / 60) . " minutos(s)";
+  }
+}
 ?>
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Certificado</title>
+  <?php require "parts/head.php"; ?>
+  <link rel="stylesheet" href="../Css/certificado.css" />
 </head>
 
 <body>
-  <h3><?= $result["nome_empresa"] ?> Certifica que</h3>
-  <h1> <?= $result["nome_usuario"] ?> </h1>
-  <h3>Completou o curso <h1><?= $result["curso_nome"] ?> </h1> com sucesso</h3>
-  <h3>em <?= $result["data_emissao"] ?></h3>
-  <p>Representado o total de <?= $result["tempo"] ?></p>
-
+  <main>
+    <h3>CERTIFICADO</h3>
+    <div class="texto">
+      A <strong><?= $result['nome_empresa'] ?></strong> e a <strong>Learnfy</strong> certifica, <strong><?= $result['nome_usuario'] ?></strong>,
+      que concluiu com êxito o curso
+      <strong><?= $result["nome_curso"] ?></strong> com uma carga horaria de <strong><?= horaOuMinutos($result["tempo"]) ?></strong> em
+      <strong><?= date_format(date_create($result["data_emissao"]), "d/n/Y") ?></strong>
+    </div>
+    <div class="baixo">
+      <img crossorigin="anonymous" src="<?= $result['logo'] ?>" alt="">
+      <div class="assinatura">
+        <p>Conexus</p>
+        <div class="barra"></div>
+        <p>Assinatura</p>
+      </div>
+      <img  class="selo" src="data:image/svg+xml;base64,<?=base64_encode($svg)?>">
+    </div>
+  </main>
 
 </body>
 
